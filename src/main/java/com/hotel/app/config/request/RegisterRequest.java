8ff36daa8f0c18febb7ae00cb2ea557db0e75c44
Kirpich <sob@mail.ru>
2023-04-18 11:41:05 +0300

    Added new checks

--- src/main/java/com/hotel/app/config/request/RegisterRequest.java
+++ src/main/java/com/hotel/app/config/request/RegisterRequest.java
@@ -1,5 +1,7 @@
 package com.hotel.app.config.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Full name is mandatory")
+    @Size(min = 10, max = 255, message = "Full name must be between 10 and 255 characters")
     private String fullName;
+    @NotBlank(message = "Email is mandatory")
+    @Size(min = 7, max = 70, message = "Email must be between 7 and 70 characters")
     private String email;
+    @NotBlank(message = "Phone number is mandatory")
+    @Size(min = 11, max = 18, message = "Phone number must be between 11 and 18 characters")
     private String phoneNumber;
+    @NotBlank(message = "Password is mandatory")
     private String password;
 }
 
